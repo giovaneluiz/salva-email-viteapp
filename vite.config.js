917994969fac3256a1ab0cbf7fb4a9e946a6606b
giovaneluiz <giovaneluiz19@yahoo.com.br>
2025-04-02 14:07:06 -0300

    ajustando configuracao para variaveis de ambiente

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3334,
-    open: true
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd())
+  return {
+    plugins: [react()],
+    server: { port: 3334 },    
   }
 })
